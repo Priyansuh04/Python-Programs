95aafc410e7ae7f4d5218ecbdada991685d8cbbf
Priyanshu Thakur <priyanshu88517@gmail.com>
2026-01-24 21:36:00 +0530

    update

--- guess.py
+++ guess.py
@@ -1,11 +1,21 @@
-import random
+# import random
 
-guess_number = random.randint(1,10)
+# guess_number = random.randint(1,10)
 
-while True:
-    user = int(input("enter the number:"))
-    if guess_number == user :
-        print("condition is true")
-        break
-    else:
-        print("condition is not true")
+# while True:
+#     user = int(input("enter the number:"))
+#     if guess_number == user :
+#         print("condition is true")
+#         break
+#     else:
+#         print("condition is not true")
+
+
+s = {1,2,3,4,5}
+lst =list(s)
+print(lst)
+lst.append(6)
+print(lst)
+print(s)
+new_set = set(lst)
+print(new_set)
