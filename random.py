d1713d6f6e0c236545906dd0ea7298dccba1c460
Priyanshu Thakur <priyanshu88517@gmail.com>
2026-01-25 20:48:00 +0530

    update

--- random.py
+++ random.py
@@ -1,7 +1,20 @@
-dict ={
-    frozenset({1,2}): 3,
-    frozenset({4,5}): 6,
-    frozenset({8,9}): 10
-}
+# dict ={
+#     frozenset({1,2}): 3,
+#     frozenset({4,5}): 6,
+#     frozenset({8,9}): 10
+# }
 
-print(dict)
+# print(dict)
+## pratical example 
+## use a dictionary to count he frequency of element in list
+
+numbers =[1,2,3,3,3,4,4,4,4]
+frequency ={}
+
+for i in numbers:
+    if i in frequency:
+        frequency[i]+=1
+    else:
+        frequency[i]=1 
+print(frequency)        
+
